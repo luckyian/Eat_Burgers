0439640595842643704dc4d014dcac309abdc104
Ian Wren <ian.wren81@gmail.com>
2020-11-25 19:28:43 -0800

    Edited boilerplate to fit project.

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,14 +1,4 @@
-/*
 
-To run this file, we do the following in our Terminal:
-
-1. Go to the directory of this sql file.
-
-2. Get into our mysql console.
-
-3. Run "source schema.sql"
-
-*/
 
 -- Drops the burgers_db if it already exists --
 DROP DATABASE IF EXISTS burgers_db;
