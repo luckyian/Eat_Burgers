798f757e63d2423d85bbcf194b21adfe02f48a25
Ian Wren <ian.wren81@gmail.com>
2021-01-17 12:23:51 -0800

    Cleaned code to reflect easier to read structure

--- server.js
+++ server.js
@@ -1,92 +1,26 @@
 var express = require("express");
-var exphbs = require("express-handlebars");
-var mysql = require("mysql");
+
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-// Parse request body as JSON
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+// Import routes and give the server access to them.
+var routes = require("./controllers/burger_controller.js");
 
-
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-});
-
-// Use Handlebars to render the main index.html page with the burgers in it.
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM burgers;", function(err, data) {
-    if (err) {
-      return res.status(500).end();
-    }
-
-    res.render("index", { burgers: data });
-  });
-});
-
-// Create a new burger
-app.post("/api/burgers", function(req, res) {
-  connection.query("INSERT INTO burgers (burger) VALUES (?)", [req.body.burgers], function(err, result) {
-    if (err) {
-      return res.status(500).end();
-    }
-
-    // Send back the ID of the new burgers
-    res.json({ id: result.id });
-    console.log({ id: result.id });
-  });
-});
-
-
-// Delete a burger
-app.delete("/api/burgers/:id", function(req, res) {
-  connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server failure
-      return res.status(500).end();
-    }
-    else if (result.affectedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    }
-    res.status(200).end();
-
-  });
-});
-
-// Add Burger to Devoured area
-// app.post("/api/devoured", function(req, res) {
-//   connection.query("INSERT INTO devoured (burger) VALUES (?)", [req.body.devoured], function(err, result) {
-//     if (err) {
-//       return res.status(500).end();
-//     }
-
-//     // Send back the ID of the new devoured burger
-//     res.json({ id: result.id });
-//     console.log({ id: result.id });
-//   });
-// });
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
