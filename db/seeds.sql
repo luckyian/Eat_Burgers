aa9ce573b26832b4f92b7a1cb5ad45c6e4052725
Ian Wren <ian.wren81@gmail.com>
2021-01-17 11:24:52 -0800

    Created schema and seed data for burger app server

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger) VALUES ('Bacon Burger', true);
