004096c7e3be5225f552214932bef699f8504c04
Ian Wren <ian.wren81@gmail.com>
2021-01-25 15:29:21 -0800

    Moved styling files to newly created public folder

---
+++ public/assets/js/click.js
@@ -0,0 +1,60 @@
+$(function() {
+    $("#devoured").on("click", function(event) {
+      var id = $(this).siblings("#devouredBurger").val();
+    
+      
+  
+      var newDevoueredState = {
+        devoured: 1
+      };
+      console.log("Update Burger", id)
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevoueredState
+      }).then(
+        function() {
+          console.log("Ate burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $("#createburger").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+      console.log("Created Burger")
+      var newBurger = {
+        burger: $("#burgerName").val().trim(),
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    // $(".delete-cat").on("click", function(event) {
+    //   var id = $(this).data("id");
+  
+    //   // Send the DELETE request.
+    //   $.ajax("/api/burgers/" + id, {
+    //     type: "DELETE"
+    //   }).then(
+    //     function() {
+    //       console.log("deleted cat", id);
+    //       // Reload the page to get the updated list
+    //       location.reload();
+    //     }
+    //   );
+    // });
+  });
+  
