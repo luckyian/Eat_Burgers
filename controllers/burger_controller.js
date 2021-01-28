57abd0877b823e30b61919f79166c96ba8cefc94
Ian Wren <ian.wren81@gmail.com>
2021-01-28 14:28:01 -0800

    Verified routes

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,11 +1,11 @@
-var express = require("express");
+const express = require("express");
 const burger = require("../models/burger");
-var router = express.Router();
+const router = express.Router();
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
     burger.all(function (data) {
-        var hbsObject = {
+        let hbsObject = {
             burgers: data
         };
         console.log(hbsObject);
