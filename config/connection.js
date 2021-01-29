5a0f50e3e073a7abb5d8da086b24553c9f8c9464
Ian Wren <ian.wren81@gmail.com>
2021-01-28 18:19:05 -0800

    Broke JAWS down to individual parameters

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,14 @@
 const mysql = require("mysql");
 let connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.NODE_ENV === "production") {
+    connection = mysql.createConnection({
+        host: process.env.JAWS_HOST,
+        port:process.env.JAWS_PORT,
+        user:process.env.JAWS_USER,
+        password:process.env.JAWS_PASS,
+        database:process.env.JAWS_DATABASE
+    });
 } else {
     connection = mysql.createConnection({
         host: "localhost",
